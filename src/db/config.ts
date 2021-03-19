5c7db1b8c2aa00f57028a06c2b972a43f4f85789
veljkoN <42590107+veljkoN@users.noreply.github.com>
2021-03-19 14:07:17 +0100

    Update config.ts

--- src/db/config.ts
+++ src/db/config.ts
@@ -2,14 +2,7 @@ import firebase from 'firebase/app'
 import 'firebase/firestore'
 import { FirebaseConfig } from '../Type_Interfaces/Interfaces'
 const firebaseConfig: FirebaseConfig = {
-  apiKey: "AIzaSyBmi5aDL8XyVfcFJOrUEKJqfkBTJb1-k9Y",
-    authDomain: "g-service-4f3ae.firebaseapp.com",
-    databaseURL: "https://g-service-4f3ae.firebaseio.com",
-    projectId: "g-service-4f3ae",
-    storageBucket: "g-service-4f3ae.appspot.com",
-    messagingSenderId: "1037726900717",
-    appId: "1:1037726900717:web:1f0ba96a512634b475c7e4",
-    measurementId: "G-EMCWV86QRL"
+  apiKey: ".........",
 }  
 firebase.initializeApp(firebaseConfig)   
   
