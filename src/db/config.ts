a9f8352b9f6df92827761ff808b993aa19574905
veljkoN <42590107+veljkoN@users.noreply.github.com>
2021-05-02 15:14:12 +0200

    Update config.ts

--- src/db/config.ts
+++ src/db/config.ts
@@ -3,9 +3,9 @@ import 'firebase/firestore'
 import { FirebaseConfig } from '../Type_Interfaces/Interfaces'
 const firebaseConfig: FirebaseConfig = {
   //DB config
-  }
-  firebase.initializeApp(firebaseConfig) 
+}
+firebase.initializeApp(firebaseConfig) 
 
-  export default firebase    
+export default firebase    
 
   
